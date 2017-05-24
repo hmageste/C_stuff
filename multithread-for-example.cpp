669ab3ff3ffde2b9ef26a9cc0629dd5b610e75a8
hmageste <henrique.mageste@gmail.com>
2017-05-24 14:45:25 +0000

    Create a multithread example using thread library

---
+++ multithread-for-example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+static const int num_of_threads = 10;
+
+void thread_method (int thread_id) {
+   std::cout << "Function running in thread " << thread_id << std::endl;
+}
+
+int main () {
+    std::thread trehads[num_of_threads];
+    for (int i = 0; i < num_of_threads; i++) {
+        threads[i] = std::thread(thread_method, i);
+     
+    std::cout << "The main function execution\n";
+    for (int i = 0; i < num_of_threads; i++)
+        threads[i].join();
+
+    return 0;   
+}
