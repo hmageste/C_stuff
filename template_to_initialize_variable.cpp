ba1cf5b6eddfee4d07d5da170601894c29342a7f
Henrique Mageste <henrique.mageste@gmail.com>
2017-11-22 16:44:46 +0000

    Example of variable templates usage

---
+++ template_to_initialize_variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+template <typename T>
+constexpr T pi = T(3.141592653589793238462643383);
+
+template <>
+constexpr const char* pi<const char*> = "pi";
+
+int main ()
+{
+    std::cout << "int pi: " << pi<int> << std::endl;
+    std::cout << "float pi: " << pi<float> << std::endl;
+    std::cout << "double pi: " << pi<double> << std::endl;
+    std::cout << "char pi: " << pi<const char*> << std::endl;
+
+    return 0;
+}
